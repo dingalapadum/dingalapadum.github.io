cef6e915758c124178b43b3966d0805a658ee88f
dingalapadum <dingalapadum@gmail.com>
2022-02-22 19:31:01 +0100

    refactor js and output ngrams to table

--- ngrams/js/ngrams.js
+++ ngrams/js/ngrams.js
@@ -1,30 +1,82 @@
-const output = document.getElementById('ngrams-output');
-
-ngrams()
-function ngrams() {
-    let gram = document.getElementById("ngrams-type").value;
-    if (gram == "characters") {
-        ngramsCharacters();
-    }else if (gram == "words") {
-        ngramsWords();
+/**
+ * Read the inputs from the ngram-example
+ * gram: is either characters
+ */
+function readNgramInputs() {
+    return {
+        gram: document.getElementById("ngrams-type").value,
+        n: +document.getElementById("n-select").value,
+        text: document.getElementById("ngrams-input").value
     }
 }
 
-function ngramsCharacters() {
-    let input = document.getElementById("ngrams-input").value;
-    let n = +document.getElementById("n-select").value;
+/**
+ * create ngrams for the text in  the input-field and output them in a table
+ */
+function ngrams() {
+    const { gram, n, text } = readNgramInputs();
+    let ngrams = null;
+    if (gram == "characters") {
+        ngrams = ngramsCharacters(n, text);
+    } else if (gram == "words") {
+        ngrams = ngramsWords(n, text);
+    } else {
+        throw "gram should be either 'characters' or 'words' - other values are not ok";
+    }
+    globalNgrams = ngrams
+    printNgramsToTable(n, globalNgrams);
+}
+
+/**
+ * @param n number of characters per 'gram'
+ * @param text to extract the n-grams from
+ * @returns Map of (ngram: count)
+ */
+function ngramsCharacters(n, text) {
     let ngrams = new Map();
-    if (n <= input.length){
-        for (let i=0; i<(input.length-n+1); i++) {
-            let currNgram = input.slice(i,i+n);
+    if (n <= text.length) {
+        for (let i = 0; i < (text.length - n + 1); i++) {
+            let currNgram = text.slice(i, i + n);
             let count = ngrams.get(currNgram) == null ? 0 : ngrams.get(currNgram);
             ngrams.set(currNgram, count + 1);
         }
     }
-    const ngramsArray = Array.from(ngrams).map(([ngram, count]) => (JSON.stringify({ngram, count})));
-    output.textContent = n + "-grams:" + ngramsArray.join(', ');
+    return ngrams;
 }
 
+/**
+ * @param n number of words per 'gram'
+ * @param text to extract the n-grams from
+ * @returns Map of (ngram: count)
+ */
 function ngramsWords() {
-    alert("hell")
+    alert("hell");
 }
+
+/**
+ * @param n is the n of n-grams
+ * @param {Map<String, Number>} ngrams
+ */
+function printNgramsToTable(n, ngrams, sortBy = "ngram") {
+    document.getElementById("ngrams-th").textContent = n + "-grams";
+
+    const ngramsArray = Array.from(ngrams).map(([ngram, count]) => ({ ngram, count }));
+    if (sortBy == "ngram") {
+        ngramsArray.sort((a, b) => a.ngram.localeCompare(b.ngram));
+    } else if (sortBy == "count") {
+        ngramsArray.sort((a, b) => a.count - b.count);
+    }
+
+    let outputTablebody = document.getElementById("ngrams-output-body");
+    while (outputTablebody.firstChild) { outputTablebody.removeChild(outputTablebody.firstChild) };
+    ngramsArray.forEach(e => {
+        let row = outputTablebody.insertRow();
+        row.insertCell(0).innerHTML = e.ngram;
+        row.insertCell(1).innerHTML = e.count;
+    })
+}
+
+/**
+ * INIT-RUN
+ */
+ngrams()
